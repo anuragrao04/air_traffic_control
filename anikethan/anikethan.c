f4a45a60257452c8bdbf64c79b0779caeacf327b
anuragrao04 <raoanu2004@gmail.com>
2023-11-20 17:48:33 +0530

    idk what on earth happened

--- anikethan/anikethan.c
+++ anikethan/anikethan.c
@@ -12,23 +12,26 @@ int checkCollision(flight_t *a, flight_t *b, float time) {
   position_t *future_position_a = calc_position(a, time);
   position_t *future_position_b = calc_position(b, time);
   double distance = calculateDistance(future_position_a, future_position_b);
+  // printf("Distance between flight %d and flight %d is %f\n", a->id, b->id,
+  // distance);
   return distance < MIN_SEPARATION_DISTANCE;
 }
 
-void resolveCollision(flight_t *a, flight_t *b, int time) {
+void resolveCollision(flight_t *a, flight_t *b, float time) {
   printf("Collision Detected! Flight %d CLIMB! flight %d DESCEND!\t YOU HAVE "
-         "%d SECONDS!\n",
+         "%f SECONDS!\n",
          a->id, b->id, time); // Move aircraft 'a' 500 units away
 }
 
-int collision_found = 0;
 void collision_avoidance(flight_t *flights[], int num_flights) {
+  int collision_found = 0;
   for (int i = 0; i < num_flights; i++) {
     for (int j = 0; j < num_flights; j++) {
       if (i == j) {
         continue; // a flight can't collide with itself
       } else {
-        for (int k = 0; k < MIN_WARNING_SECONDS; k++) {
+        for (float k = 0; k < MIN_WARNING_SECONDS; k++) {
+          // printf("%f\n", k);
           if (checkCollision(flights[i], flights[j], k)) {
             resolveCollision(flights[i], flights[j], k);
             collision_found = 1;
--- anujna/anujna.c
+++ anujna/anujna.c
@@ -5,10 +5,12 @@
 
 position_t *calc_position(flight_t *flight, float time) {
   position_t *future_position = malloc(sizeof(position_t));
-  float distance;
+  int distance;
   distance = time * flight->speed;
   future_position->x = distance * cos(flight->heading) + flight->x;
   future_position->y = distance * sin(flight->heading) + flight->y;
   future_position->z = flight->z;
+  // printf("Flight:%d %f %f %f\n", flight->id, future_position->x,
+  // future_position->y, future_position->z);
   return future_position;
 }
